c0a14c0496d30778e9f41879fdf800fd9ea3abb8
colino17 <51090962+colino17@users.noreply.github.com>
2021-01-27 15:39:10 -0400

    Replace if HIGH with else releaseAll and add Discord Combo for Macro 1

--- macropad.ino
+++ macropad.ino
@@ -9,49 +9,34 @@
 #define MACRO_4 7
 
 void setup() {
-  pinMode (MACRO_1, INPUT_PULLUP);
-  pinMode (MACRO_2, INPUT_PULLUP);
-  pinMode (MACRO_3, INPUT_PULLUP);
-  pinMode (MACRO_4, INPUT_PULLUP);
+  pinMode(MACRO_1, INPUT_PULLUP);
+  pinMode(MACRO_2, INPUT_PULLUP);
+  pinMode(MACRO_3, INPUT_PULLUP);
+  pinMode(MACRO_4, INPUT_PULLUP);
 
   Keyboard.begin();
 }
 
 void loop() {
-  // MACRO_1 //
-  if (digitalRead(MACRO_1) == LOW)
-  {
-    Keyboard.press(KEY_LEFT_ALT);
-  }
-  if (digitalRead(MACRO_1) == HIGH)
-    {
-     Keyboard.release(KEY_LEFT_ALT);
-  }
-  // MACRO_2 //
-  if (digitalRead(MACRO_2) == LOW)
-  {
-    Keyboard.press(KEY_UP_ARROW);
-  }
-  if (digitalRead(MACRO_2) == HIGH)
-  {
-    Keyboard.release(KEY_UP_ARROW);
-  }
-  // MACRO_3 //
-  if (digitalRead(MACRO_3) == LOW)
-  {
+  // MACRO_1 - DISCORD MUTE //
+  if (digitalRead(MACRO_1) == LOW) {
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press(KEY_LEFT_SHIFT);
     Keyboard.press(KEY_LEFT_CTRL);
   }
-  if (digitalRead(MACRO_3)==HIGH)
-  {
-    Keyboard.release(KEY_LEFT_CTRL);
+  // MACRO_2 //
+  if (digitalRead(MACRO_2) == LOW) {
+    Keyboard.press(KEY_UP_ARROW);
+  }
+  // MACRO_3 //
+  if (digitalRead(MACRO_3) == LOW) {
+    Keyboard.press(KEY_LEFT_CTRL);
   }
   // MACRO_4 //
-  if (digitalRead(MACRO_4) == LOW)
-  {
+  if (digitalRead(MACRO_4) == LOW) {
     Keyboard.press(KEY_LEFT_ARROW);
   }
-  if (digitalRead(MACRO_4) == HIGH)
-  {
-    Keyboard.release(KEY_LEFT_ARROW);
+  else {
+    Keyboard.releaseAll();
   }
-  }
+}
