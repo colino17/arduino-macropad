670f3b428ca36951c44933146e1144e33f133c8c
colino17 <51090962+colino17@users.noreply.github.com>
2021-01-28 11:14:30 -0400

    Create cleanupdate.sh

---
+++ scripts/cleanupdate.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+############
+# PASSWORD #
+############
+read -s -p "Please enter your password : " PASSWORD
+echo "  "
+
+###################
+# UPDATING SYSTEM #
+###################
+echo -e "Updating your system..."
+sleep 3
+
+echo $PASSWORD | sudo -S pacman -Syu 2> /dev/null
+
+echo $PASSWORD | sudo -S yay -Syu 2> /dev/null
+
+###############################
+# CLEANUP PACMAN DEPENDENCIES #
+###############################
+echo -e "Removing Orphaned Packages..."
+sleep 3
+
+echo $PASSWORD | sudo -S pacman -Rns $(pacman -Qtdq) --noconfirm 2> /dev/null
+
+############################
+# CLEANUP AUR DEPENDENCIES #
+############################
+echo -e "Removing Unused AUR dependencies..."
+sleep 3
+
+echo $PASSWORD | sudo -S yay -Yc --noconfirm  2> /dev/null
+
+echo -e "Cleanup is complete!"
+sleep 3
+exit
