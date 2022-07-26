18d87c49dc912daee865d51ab3037d23a80d6748
colino17 <51090962+colino17@users.noreply.github.com>
2022-07-26 13:50:27 -0300

    Update macropad.ino

--- macropad.ino
+++ macropad.ino
@@ -22,28 +22,32 @@ void loop() {
 // This would be an option if the catchall release is not effective 
   // MACRO_1 - DISCORD MUTE - i3 KEYCODE 198 //
   if (digitalRead(MACRO_1) == LOW) {
-    Keyboard.write(KEY_F20);
+    Keyboard.press(KEY_LEFT_SHIFT);
+    Keyboard.press(KEY_F20);
     delay(5);
     Keyboard.releaseAll();
     delay(2000);
   }
   // MACRO_2 - i3 KEYCODE 199 //
   if (digitalRead(MACRO_2) == LOW) {
-    Keyboard.write(KEY_F21);
+    Keyboard.press(KEY_LEFT_SHIFT);
+    Keyboard.press(KEY_F21);
     delay(5);
     Keyboard.releaseAll();
     delay(2000);
   }
   // MACRO_3 - VM STARTUP - i3 KEYCODE 200//
   if (digitalRead(MACRO_3) == LOW) {
-    Keyboard.write(KEY_F22);
+    Keyboard.press(KEY_LEFT_SHIFT);
+    Keyboard.press(KEY_F22);
     delay(5);
     Keyboard.releaseAll();
     delay(2000);
   }
   // MACRO_4 - UPDATE DISTRO - i3 KEYCODE 201//
   if (digitalRead(MACRO_4) == LOW) {
-    Keyboard.write(KEY_F23);
+    Keyboard.press(KEY_LEFT_SHIFT);
+    Keyboard.press(KEY_F23);
     delay(5);
     Keyboard.releaseAll();
     delay(2000);
